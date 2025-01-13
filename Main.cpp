9d5e1f9d67a0750e1716c84312a72091cc24ebdf
IsaiahNaden <isaiahnaden22@gmail.com>
2025-01-13 12:56:55 +0800

    modified code to read sample2 and sample3

--- Main.cpp
+++ Main.cpp
@@ -5,14 +5,18 @@
 #include <vector>
 using namespace std;
 
+
 int main()
 {
     ifstream inFile;
+    ifstream inFile2;
+    ifstream inFile3;
+
     inFile.open("Sample1.txt");
     vector<string> lines;   // Vector to store lines
     string line;
 
-    if (!inFile.is_open()) 
+    if (!inFile.is_open()) // validating file input
     {
         cout << "Error: Could not open the file.\n";
         return 1;
@@ -26,26 +30,117 @@ int main()
     inFile.close(); // Close the file
 
     // Print the lines
-    cout << "Lines in the file:\n";
+    cout << "Lines in Sample1:\n";
     for (const string& l : lines) 
     {
         cout << l << "\n";
     }
 
-    ofstream outFile("output1.txt");
+    ofstream outFile("output1.txt"); //outputting to file
     if (outFile.is_open())
     {
         outFile << lines[0] << endl;
         outFile << lines[1] << endl;
-        outFile << "C:\\Users\\User\\OneDrive - mmu.edu.my\\Documents\\C++\\assignment\\Sample1.txt" << endl;
+        outFile << endl; // code to add file path from user directory
         cout << "File created and written successfully!";
+        cout << endl;
+        cout << endl;
+        cout << endl;
     }
 
     else
     {
-        cerr << "Error, unable to open file.";
+        cerr << "Error, unable to open file."; 
     }
     
     outFile.close();
+
+    inFile2.open("Sample2.txt");
+    vector<string> lines2;
+    string line2;
+
+    if (!inFile2.is_open()) // validating file input
+    {
+        cout << "Error: Could not open the file.\n";
+        return 1;
+    }
+
+    while (getline(inFile2, line2)) 
+    {
+        lines2.push_back(line2);
+    }
+
+    inFile2.close(); // Close the file
+
+    // Print the lines
+    cout << "Lines in Sample2:\n";
+    for (const string& l : lines2) 
+    {
+        cout << l << "\n";
+    }
+
+    ofstream outFile2("output2.txt"); //outputting to file
+    if (outFile2.is_open())
+    {
+        outFile2 << lines2[0] << endl;
+        outFile2 << lines2[1] << endl;
+        outFile2 << endl; // code to add file path from user directory
+        cout << "File created and written successfully!";
+        cout << endl;
+        cout << endl;
+        cout << endl;
+    }
+
+    else
+    {
+        cerr << "Error, unable to open file."; 
+    }
+    
+    outFile2.close();
+
+    
+    inFile3.open("Sample3.txt");
+    vector<string> lines3;
+    string line3;
+
+    if (!inFile3.is_open()) // validating file input
+    {
+        cout << "Error: Could not open the file.\n";
+        return 1;
+    }
+
+    while (getline(inFile3, line3)) 
+    {
+        lines3.push_back(line3);
+    }
+
+    inFile3.close(); // Close the file
+
+    // Print the lines
+    cout << "Lines in Sample3:\n";
+    for (const string& l : lines3) 
+    {
+        cout << l << "\n";
+    }
+
+    ofstream outFile3("output3.txt"); //outputting to file
+    if (outFile3.is_open())
+    {
+        outFile3 << lines3[0] << endl;
+        outFile3 << lines3[1] << endl;
+        outFile3 << endl; // code to add file path from user directory
+        cout << "File created and written successfully!";
+        cout << endl;
+        cout << endl;
+        cout << endl;
+    }
+
+    else
+    {
+        cerr << "Error, unable to open file."; 
+    }
+    
+    outFile3.close();
+
     return 0;
 }
